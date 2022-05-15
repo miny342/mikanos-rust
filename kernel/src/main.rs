3322773ee48c769fff85e3641fe11d0cf5dfd24a
miny342 <keto1320xeon@gmail.com>
2022-05-15 15:07:28 +0900

    draw A

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -18,6 +18,38 @@ fn panic(_info: &PanicInfo) -> ! {
     }
 }
 
+const kFontA: [u8; 16] = [
+    0b00000000, //
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b01111110, //  ******
+    0b01000010, //  *    *
+    0b01000010, //  *    *
+    0b01000010, //  *    *
+    0b11100111, // ***  ***
+    0b00000000, //
+    0b00000000, //
+];
+
+fn write_ascii(writer: &PixelWriter, x: usize, y: usize, c: char, color: &PixelColor) {
+    if c != 'A' {
+        return
+    }
+    for dy in 0..16 {
+        for dx in 0..8 {
+            if (kFontA[dy] << dx) & 0x80 != 0 {
+                writer.write(x + dx, y + dy, color);
+            }
+        }
+    }
+}
+
 #[no_mangle]
 extern "efiapi" fn kernel_main(config: *const FrameBufferConfig) -> ! {
     let writer = PixelWriter::new(unsafe {*config});
@@ -31,6 +63,10 @@ extern "efiapi" fn kernel_main(config: *const FrameBufferConfig) -> ! {
             writer.write(100 + x, 100 + y, &PixelColor { r: 255, g: 0, b: 255 });
         }
     }
+
+    let color = PixelColor { r: 0, g: 0, b: 0};
+    write_ascii(&writer, 50, 50, 'A', &color);
+    write_ascii(&writer, 58, 50, 'A', &color);
 
     loop {
         unsafe {
